e12e43450b1eb53c4da5ee6886eba9c3e96482ef
Bonnie <bonniepacker99@gmail.com>
2025-02-26 09:41:02 +0000

    edit formatting for lambda function

---
+++ src/cloudwatch.py
@@ -0,0 +1,3 @@
+import boto3
+
+sns_client = boto3.client("sns", region_name="eu-west-2")
--- src/extract_lambda.py
+++ src/extract_lambda.py
@@ -1,17 +1,30 @@
-""" Function should query the DB and format data into JSON file"""
+"""Function should query the DB and format data into JSON file"""
 
 from GetSecrets.connect_db import connect_to_database
-from pprint import pprint
 import json
 import boto3
 from datetime import datetime
+
 """ Function should query the DB and format data into JSON file"""
+
+
 def lambda_handler():
     db = connect_to_database()
-    table_list = ["counterparty,currency","department","design","staff","sales_order","address","payment","purchase_order","payment_type","transaction"]
+    table_list = [
+        "counterparty,currency",
+        "department",
+        "design",
+        "staff",
+        "sales_order",
+        "address",
+        "payment",
+        "purchase_order",
+        "payment_type",
+        "transaction",
+    ]
     for table in table_list:
-        query_string = f"SELECT * FROM {table} WHERE " # <----finish this bit and store the last ingested time
-        data =db.run(query_string)
+        query_string = f"SELECT * FROM {table} WHERE "
+        data = db.run(query_string)
         with db.cursor() as cursor:
             cursor.execute(query_string)
             data = cursor.fetchall()
@@ -19,11 +32,13 @@ def lambda_handler():
         formatted = [dict(zip(columns, data)) for column in columns]
         time = datetime.now()
         print(time)
-        s3_client = boto3.client('s3')
+        s3_client = boto3.client("s3")
         s3_client.put_object(
-                    Bucket="testbucket123abc456def",
-                    Key=f'testdate/testtime/{table}',  # < ----- CHANGE THE FILE PATHS HERE
-                    Body=json.dumps(formatted, indent=4, sort_keys=True, default=str),
-                    ContentType='application/json')
-lambda_handler()
+            Bucket="testbucket123abc456def",
+            Key=f"testdate/testtime/{table}",
+            Body=json.dumps(formatted, indent=4, sort_keys=True, default=str),
+            ContentType="application/json",
+        )
 
+
+lambda_handler()
