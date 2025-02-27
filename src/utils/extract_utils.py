e86b68f41e14cc20c5244c2e91a9ab5b6e6e3dd2
pieterjvdb <pieterjvdb@gmail.com>
2025-02-27 15:10:48 +0000

    adding testing and functionality for formatting filepath

---
+++ src/utils/extract_utils.py
@@ -0,0 +1,33 @@
+def format_filepath(date_time, table):
+    
+    months = {
+            "01": "January",
+            "02": "February",
+            "03": "March",
+            "04": "April",
+            "05": "May",
+            "06": "June",
+            "07": "July",
+            "08": "August",
+            "09": "September",
+            "10": "October",
+            "11": "November",
+            "12": "December",
+        }
+
+    split = date_time.split("-")
+    year = split[0]
+    month = split[1]
+    split2 = split[2].split(" ")
+    day = split2[0]
+    time = split2[1]
+
+    monthstr = f"{month}-{months[month]}"
+
+    filepath = f"data/by time/{year}/{monthstr}/{day}/{time}/{table}"
+
+    return filepath
+
+
+def get_data():
+    pass
---
+++ tests/test_extract_utils.py
@@ -0,0 +1,15 @@
+from src.utils.extract_utils import format_filepath, get_data
+
+def test_filepath_as_expected():
+    test_date_time = '2000-01-01 12:00:00.000000'
+    test_table = 'table'
+    #check num dps
+    expected = 'data/by time/2000/01-January/01/12:00:00.000000/table'
+    assert format_filepath(test_date_time, test_table) == expected
+
+
+# def test_get_data_returns_expected():
+#     expected = [{'staff_id': 1, 'first_name' : 'A', 'last_name': 'Name', 'department_id': 1,
+#                  'email_address': 'test@gmail.com', 'created_at': "2025", 'last_updated': '2025'},
+#                  {'staff_id': 2, 'first_name' : 'B', 'last_name': 'Nam', 'department_id': 3,
+#                  'email_address': 'test1@gmail.com', 'created_at': "2021", 'last_updated': '2028'}]
