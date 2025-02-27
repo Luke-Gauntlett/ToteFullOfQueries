74851149fdfae64f0a755b6a2ccbc51cbee65554
pieterjvdb <pieterjvdb@gmail.com>
2025-02-27 15:15:56 +0000

    formatting

--- src/utils/extract_utils.py
+++ src/utils/extract_utils.py
@@ -1,19 +1,19 @@
 def format_filepath(date_time, table):
-    
+
     months = {
-            "01": "January",
-            "02": "February",
-            "03": "March",
-            "04": "April",
-            "05": "May",
-            "06": "June",
-            "07": "July",
-            "08": "August",
-            "09": "September",
-            "10": "October",
-            "11": "November",
-            "12": "December",
-        }
+        "01": "January",
+        "02": "February",
+        "03": "March",
+        "04": "April",
+        "05": "May",
+        "06": "June",
+        "07": "July",
+        "08": "August",
+        "09": "September",
+        "10": "October",
+        "11": "November",
+        "12": "December",
+    }
 
     split = date_time.split("-")
     year = split[0]
