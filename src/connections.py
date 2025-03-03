1e75e0ea0c20f477af84eff78a7ff5826d6f0989
Luke-Gauntlett <luke.gauntlett@hotmail.co.uk>
2025-03-03 09:01:49 +0000

    nosec on the credentials

--- src/connections.py
+++ src/connections.py
@@ -24,8 +24,8 @@ def connect_to_database():
     """
 
     try:
-        secret_name = "project_database_credentials" 
-        region_name = "eu-west-2"
+        secret_name = "project_database_credentials"  #nosec
+        region_name = "eu-west-2"                     #nosec  
 
         credentials = get_db_credentials(secret_name, region_name)
 
