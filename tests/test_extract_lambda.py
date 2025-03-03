7d78c4634442c0b1cdff140760e7a05bb43f54a9
Luke-Gauntlett <luke.gauntlett@hotmail.co.uk>
2025-03-03 09:35:30 +0000

    aaa

--- tests/test_extract_lambda.py
+++ tests/test_extract_lambda.py
@@ -9,14 +9,14 @@ from datetime import datetime
 
 
 
-@pytest.fixture(scope="function")
-def aws_credentials():
-    """Mocked AWS Credentials for moto"""
-    os.environ["AWS_ACCESS_KEY_ID"] = "test"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
-    os.environ["AWS_SECURITY_TOKEN"] = "test"
-    os.environ["AWS_SESSION_TOKEN"] = "test"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+# @pytest.fixture(scope="function")
+# def aws_credentials():
+#     """Mocked AWS Credentials for moto"""
+#     os.environ["AWS_ACCESS_KEY_ID"] = "test"
+#     os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+#     os.environ["AWS_SECURITY_TOKEN"] = "test"
+#     os.environ["AWS_SESSION_TOKEN"] = "test"
+#     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
 
 def test_time_no_file(aws_credentials):
