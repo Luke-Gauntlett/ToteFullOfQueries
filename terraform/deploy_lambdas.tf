d13d183a1e6ccc9c71947c1eea96827e801d103e
Bonnie <bonniepacker99@gmail.com>
2025-03-05 16:35:05 +0000

    update lambda so it can deploy successfully

--- terraform/deploy_lambdas.tf
+++ terraform/deploy_lambdas.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "totes_extract_lambda" {
   timeout          = var.default_timeout
   memory_size      = 512  # Increase from 128 MB to 512 MB
   layers           = [
-    aws_lambda_layer_version.dependencieslayer1version.arn,
+    aws_lambda_layer_version.dependencieslayer.arn,
     aws_lambda_layer_version.utilslayerversion.arn
   ]
 }
@@ -17,13 +17,13 @@ resource "aws_lambda_function" "totes_transform_lambda" {
   function_name    = var.transform_lambda
   source_code_hash = data.archive_file.transform_lambda.output_base64sha256
   filename         = data.archive_file.transform_lambda.output_path
-  role             = aws_iam_role.lambda_iam.arn
+  role             = aws_iam_role.lambda_iam2.arn
   handler          = "${var.transform_lambda}.lambda_handler"
   runtime          = "python3.12"
   timeout          = var.default_timeout
   memory_size      = 512
   layers           = [
-    aws_lambda_layer_version.dependencieslayer2version.arn,
+    aws_lambda_layer_version.dependencieslayer.arn,
     aws_lambda_layer_version.utilslayerversion.arn,
     "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:16"]
 }
--- terraform/lambda_layer.tf
+++ terraform/lambda_layer.tf
@@ -14,23 +14,13 @@ data "archive_file" "transform_lambda" {
 
 ####################### zips the layer folders #########################################
 
-data "archive_file" "dependencieslayer1" {
+data "archive_file" "dependencieslayer" {
   type        = "zip"
-  source_dir  = "${path.module}/../deploy/layers/unziped/dependencies1"
-  output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer1/dependencieslayer1.zip"
+  source_dir  = "${path.module}/../deploy/layers/unzipped/dependencies"
+  output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer.zip"
 }
 
-data "archive_file" "dependencieslayer2" {
-  type        = "zip"
-  source_dir  = "${path.module}/../deploy/layers/unziped/dependencies2"
-  output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer2/dependencieslayer2.zip"
-}
 
-# data "archive_file" "dependencieslayer3" {
-#   type        = "zip"
-#   source_dir  = "${path.module}/../deploy/layers/unziped/dependencies3"
-#   output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer3/dependencieslayer3.zip"
-# }
 
 data "archive_file" "utilslayer" {
   type        = "zip"
@@ -47,26 +37,13 @@ resource "aws_lambda_layer_version" "utilslayerversion"{
   depends_on = [null_resource.force_redeploy]
 }
 
-resource "aws_lambda_layer_version" "dependencieslayer1version" {
-  layer_name = "dependencieslayer1"
-  filename = data.archive_file.dependencieslayer1.output_path
+resource "aws_lambda_layer_version" "dependencieslayer" {
+  layer_name = "dependencieslayer"
+  filename = data.archive_file.dependencieslayer.output_path
   compatible_runtimes = ["python3.12"]
   depends_on = [null_resource.force_redeploy]
 }
 
-resource "aws_lambda_layer_version" "dependencieslayer2version" {
-  layer_name = "dependencieslayer2"
-  filename = data.archive_file.dependencieslayer2.output_path
-  compatible_runtimes = ["python3.12"]
-  depends_on = [null_resource.force_redeploy]
-}
-
-# resource "aws_lambda_layer_version" "dependencieslayer3version" {
-#   layer_name = "dependencieslayer3"
-#   filename = data.archive_file.dependencieslayer3.output_path
-#   compatible_runtimes = ["python3.12"]
-#   depends_on = [null_resource.force_redeploy]
-# }
 
 ############### triggers to remake  lambdas and layers ###########################################
 resource "null_resource" "force_redeploy" {
