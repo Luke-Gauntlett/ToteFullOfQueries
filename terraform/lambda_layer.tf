a26695891adc03d1f2c9967bd08a0f8adc2934ef
Luke-Gauntlett <luke.gauntlett@hotmail.co.uk>
2025-03-05 15:13:05 +0000

    kindaworkingterraform

--- terraform/lambda_layer.tf
+++ terraform/lambda_layer.tf
@@ -1,64 +1,76 @@
-#making the zip file for the extract lambda function
+#################### zips the lambdas ##############################################
+
 data "archive_file" "extract_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/extract_lambda.py"
-  output_path = "${path.module}/../packages/extract_lambda/function.zip"
+  output_path = "${path.module}/../deploy/lambdas/extract_lambda.zip"
 }
 
-#making the zip file for the extract lambda function
 data "archive_file" "transform_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/transform_lambda.py"
-  output_path = "${path.module}/../packages/transform_lambda/function.zip"
+  output_path = "${path.module}/../deploy/lambdas/transform_lambda.zip"
 }
 
-# making the zip file for lambda layer
-data "archive_file" "layer_code" {
+####################### zips the layer folders #########################################
+
+data "archive_file" "dependencieslayer1" {
   type        = "zip"
-  output_path = "${path.module}/../packages/extract_lambda/layer.zip"
-  source_dir  = "${path.module}/../dependencies"
+  source_dir  = "${path.module}/../deploy/layers/unziped/dependencies1"
+  output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer1/dependencieslayer1.zip"
 }
 
-data "archive_file" "connection_resource" {
+data "archive_file" "dependencieslayer2" {
   type        = "zip"
-  source_dir = "${path.module}/../GetSecrets"
-  output_path = "${path.module}/../connection_resource/connections.zip"
+  source_dir  = "${path.module}/../deploy/layers/unziped/dependencies2"
+  output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer2/dependencieslayer2.zip"
 }
 
-# lambda zip code being put in the above bucket
-resource "aws_s3_object" "lambda_zip_code" {
-  for_each = toset([var.extract_lambda,var.transform_lambda])
-  # add var.transform_lambda, var.load_lambda above when ready
-  # currently waiting for vars file to be completed
-  bucket = aws_s3_bucket.code_bucket.bucket
-  key    = "${each.key}/function.zip"
-  source = "${path.module}/../packages/${each.key}/function.zip"
-  # each.key loops over items in for_each at the top
-  etag = filemd5("${path.module}/../packages/${each.key}/function.zip")
+# data "archive_file" "dependencieslayer3" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/../deploy/layers/unziped/dependencies3"
+#   output_path = "${path.module}/../deploy/layers/zipped/dependencieslayer3/dependencieslayer3.zip"
+# }
+
+data "archive_file" "utilslayer" {
+  type        = "zip"
+  source_dir = "${path.module}/../utils"
+  output_path = "${path.module}/../deploy/layers/zipped/utilslayer/utilslayer.zip"
 }
 
-# lambda layer code uploading into the bucket
-resource "aws_s3_object" "lambda_layer" {
-  bucket     = aws_s3_bucket.code_bucket.bucket
-  key        = "layer/layer.zip"
-  source     = data.archive_file.layer_code.output_path
-  etag       = filemd5(data.archive_file.layer_code.output_path)
-  depends_on = [data.archive_file.layer_code]
-}
+###################################### make layers ######################################################
 
-# Layer to zip connection to secrets manager and db
-resource "aws_lambda_layer_version" "connection_resource_layer"{
-  layer_name = "connection_resource_layer"
-  description = "Layer to add connect to secrets manager and db"
-  filename = data.archive_file.connection_resource.output_path
+resource "aws_lambda_layer_version" "utilslayerversion"{
+  layer_name = "utilslayer"
+  filename = data.archive_file.utilslayer.output_path
   compatible_runtimes = ["python3.12"]
+  depends_on = [null_resource.force_redeploy]
 }
 
-# references the layer zip code in the bucket, for the main lambda function
-# to point to under the key of 'layers'
-resource "aws_lambda_layer_version" "dependencies" {
-  layer_name = "requirements_layer"
-  s3_bucket  = aws_s3_object.lambda_layer.bucket
-  s3_key     = aws_s3_object.lambda_layer.key
+resource "aws_lambda_layer_version" "dependencieslayer1version" {
+  layer_name = "dependencieslayer1"
+  filename = data.archive_file.dependencieslayer1.output_path
+  compatible_runtimes = ["python3.12"]
+  depends_on = [null_resource.force_redeploy]
 }
 
+resource "aws_lambda_layer_version" "dependencieslayer2version" {
+  layer_name = "dependencieslayer2"
+  filename = data.archive_file.dependencieslayer2.output_path
+  compatible_runtimes = ["python3.12"]
+  depends_on = [null_resource.force_redeploy]
+}
+
+# resource "aws_lambda_layer_version" "dependencieslayer3version" {
+#   layer_name = "dependencieslayer3"
+#   filename = data.archive_file.dependencieslayer3.output_path
+#   compatible_runtimes = ["python3.12"]
+#   depends_on = [null_resource.force_redeploy]
+# }
+
+############### triggers to remake  lambdas and layers ###########################################
+resource "null_resource" "force_redeploy" {
+  triggers = {
+    always_run = timestamp()
+  }
+}
