f7004f4f6645628d27ab15be08e0494ac8c0a883
Luke-Gauntlett <luke.gauntlett@hotmail.co.uk>
2025-03-06 12:11:44 +0000

    changingtimes

--- terraform/event_bridge.tf
+++ terraform/event_bridge.tf
@@ -1,24 +1,40 @@
-resource "aws_cloudwatch_event_rule" "scheduler" {
-  name                = "lambda-scheduler"
-  description         = "Trigger Lambda every 5 minutes"
+# resource "aws_cloudwatch_event_rule" "scheduler" {
+#   name                = "lambda-scheduler"
+#   description         = "Trigger Lambda every 5 minutes"
+#   schedule_expression = "rate(5 minutes)"
+#   state               = "DISABLED"
+#   tags = {
+#     Environment = "dev"
+#   }
+# }
+
+# ## Create the rule
+# resource "aws_cloudwatch_event_target" "scheduler_target" {
+#   rule      = aws_cloudwatch_event_rule.scheduler.name
+#   target_id = "lambda-target"
+#   arn       = aws_lambda_function.totes_extract_lambda.arn
+# }
+
+resource "aws_scheduler_schedule" "scheduler" {
+  name       = "my_first_terraform_scheduler"
+  flexible_time_window {
+    mode = "OFF"
+  }
   schedule_expression = "rate(5 minutes)"
-  state               = "DISABLED"
-  tags = {
-    Environment = "dev"
+
+  target {
+    arn      = aws_sfn_state_machine.totes_step_function.arn
+    role_arn = aws_iam_role.step_function_role.arn
   }
 }
-## Create the rule
-resource "aws_cloudwatch_event_target" "scheduler_target" {
-  rule      = aws_cloudwatch_event_rule.scheduler.name
-  target_id = "lambda-target"
-  arn       = aws_lambda_function.totes_extract_lambda.arn
-}
 
-#### Allow permission to lambda
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.totes_extract_lambda.arn
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.scheduler.arn
-}
+# #### Allow permission to lambda
+# resource "aws_lambda_permission" "allow_cloudwatch" {
+#   statement_id  = "AllowExecutionFromCloudWatch"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.totes_extract_lambda.arn
+#   principal     = "events.amazonaws.com"
+#   source_arn    = aws_cloudwatch_event_rule.scheduler.arn
+# }
+
+
