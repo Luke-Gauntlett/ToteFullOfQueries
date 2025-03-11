b7131435ec09785f21e025ec3e0433a8fe5b1d37
Bonnie <bonniepacker99@gmail.com>
2025-03-11 13:27:11 +0000

    edit for flake8 testing

--- tests/test_extract_lambda.py
+++ tests/test_extract_lambda.py
@@ -4,7 +4,6 @@ import pytest
 import boto3
 from unittest.mock import Mock, patch
 from moto import mock_aws
-from datetime import datetime
 from botocore.exceptions import ClientError
 from src.extract_lambda import lambda_handler, get_time, write_data
 import logging
@@ -34,7 +33,7 @@ def mock_client(aws_credentials):
 def mock_db():
     mock_db = Mock()
     mock_db.run.return_value = [("column1",), ("column2",)]
-    
+
     return mock_db
 
 
@@ -51,40 +50,40 @@ class TestExtractLambda:
                 "Body": Mock(read=Mock(return_value=b'["2025-03-11 10:00:00.000000"]'))
             }
 
-            
             mock_db_instance = Mock()
             mock_db.return_value = mock_db_instance
-            mock_db_instance.run.return_value = [("column1",), ("column2",)]  
+            mock_db_instance.run.return_value = [("column1",), ("column2",)]
 
             event, context = {}, Mock()
 
-            
             result = lambda_handler(event, context)
 
             assert "filepaths" in result
             assert isinstance(result["filepaths"], list)
-            assert len(result["filepaths"]) > 0 
+            assert len(result["filepaths"]) > 0
 
-            mock_s3.put_object.assert_called() 
+            mock_s3.put_object.assert_called()
 
-            expected_query = "SELECT column_name FROM information_schema.columns WHERE table_name = :table_name ORDER BY ordinal_position"
+            expected_query = """ SELECT column_name FROM information_schema.columns
+            WHERE table_name = :table_name ORDER BY ordinal_position """
             actual_query = mock_db_instance.run.call_args_list[0][0][0].strip()
-            actual_query = " ".join(actual_query.split())  
+            actual_query = " ".join(actual_query.split())
 
-            
             assert expected_query == actual_query
 
-            
-            expected_data_query = "SELECT * FROM counterparty WHERE created_at > :last_extract_time OR last_updated > :last_extract_time"
+            expected_data_query = """SELECT * FROM counterparty
+            WHERE created_at > :last_extract_time OR last_updated > :last_extract_time"""
             actual_data_query = mock_db_instance.run.call_args_list[1][0][0].strip()
-            actual_data_query = " ".join(actual_data_query.split())  
+            actual_data_query = " ".join(actual_data_query.split())
 
             assert expected_data_query == actual_data_query
-                    
+
 
 class TestGetTime:
     def test_get_time_no_existing_file(self, mock_client):
-        last_extraction_time, this_extraction_time = get_time(mock_client, bucketname="test_bucket")
+        last_extraction_time, this_extraction_time = get_time(
+            mock_client, bucketname="test_bucket"
+        )
         assert last_extraction_time == "0001-01-01 00:00:00.000000"
         assert isinstance(this_extraction_time, str)
 
@@ -94,43 +93,50 @@ class TestGetTime:
             Bucket="test_bucket",
             Key="last_extraction_times.json",
             Body=json.dumps(last_times),
-            ContentType="application/json"
+            ContentType="application/json",
         )
-        last_extraction_time, this_extraction_time = get_time(mock_client, bucketname="test_bucket")
+        last_extraction_time, this_extraction_time = get_time(
+            mock_client, bucketname="test_bucket"
+        )
         assert last_extraction_time == "2025-02-24 12:00:00.000000"
         assert isinstance(this_extraction_time, str)
-
 
 
 class TestWriteData:
 
     @patch("src.extract_lambda.connect_to_database")
     def test_write_data_no_updates(self, mock_db, mock_client):
-        """Test write_data when there are no updates (should still return file paths for all tables)."""
-        
-        mock_db.run.return_value = []  
-        
-        
-        result = write_data("2025-02-24 12:00:00.000000", "2025-02-25 12:00:00.000000", mock_client, mock_db, bucketname="test_bucket")
-        
-        
+        """Test write_data when there are no updates
+        (should still return file paths for all tables)."""
+
+        mock_db.run.return_value = []
+
+        result = write_data(
+            "2025-02-24 12:00:00.000000",
+            "2025-02-25 12:00:00.000000",
+            mock_client,
+            mock_db,
+            bucketname="test_bucket",
+        )
+
         assert "filepaths" in result
         assert isinstance(result["filepaths"], list)
-        assert len(result["filepaths"]) > 0 
+        assert len(result["filepaths"]) > 0
         assert result["filepaths"] == [
-            'data/by time/2025/02-February/25/12:00:00.000000/counterparty',
-            'data/by time/2025/02-February/25/12:00:00.000000/currency',
-            'data/by time/2025/02-February/25/12:00:00.000000/department',
-            'data/by time/2025/02-February/25/12:00:00.000000/design',
-            'data/by time/2025/02-February/25/12:00:00.000000/staff',
-            'data/by time/2025/02-February/25/12:00:00.000000/sales_order',
-            'data/by time/2025/02-February/25/12:00:00.000000/address'
+            "data/by time/2025/02-February/25/12:00:00.000000/counterparty",
+            "data/by time/2025/02-February/25/12:00:00.000000/currency",
+            "data/by time/2025/02-February/25/12:00:00.000000/department",
+            "data/by time/2025/02-February/25/12:00:00.000000/design",
+            "data/by time/2025/02-February/25/12:00:00.000000/staff",
+            "data/by time/2025/02-February/25/12:00:00.000000/sales_order",
+            "data/by time/2025/02-February/25/12:00:00.000000/address",
         ]
 
-    
 
 class TestCloudWatchLogging:
-    def test_get_time_logs_correct_text_for_extraction_time_error(self, caplog, aws_credentials):
+    def test_get_time_logs_correct_text_for_extraction_time_error(
+        self, caplog, aws_credentials
+    ):
         with mock_aws():
             client = boto3.client("s3")
 
@@ -142,7 +148,9 @@ class TestCloudWatchLogging:
                 get_time(client, bucketname="testingBucket")
                 assert "No most recent extraction time." in caplog.text
 
-    def test_get_time_logs_correct_text_for_any_other_error(self, caplog, aws_credentials):
+    def test_get_time_logs_correct_text_for_any_other_error(
+        self, caplog, aws_credentials
+    ):
         with mock_aws():
             client = boto3.client("s3")
 
@@ -155,12 +163,15 @@ class TestCloudWatchLogging:
                     get_time(client, bucketname="noBucket")
                 assert "Error! Issues getting extraction time." in caplog.text
 
-
     def test_write_data_logs_correct_text(self, mock_client, mock_db, caplog):
         last_extraction_time = "0001-01-01 00:00:00.000000"
         this_extraction_time = "0001-01-03 00:00:00.000000"
         with caplog.at_level(logging.INFO):
-            write_data(last_extraction_time, this_extraction_time, mock_client, mock_db, bucketname="test_bucket") # noqa
+            write_data(
+                last_extraction_time,
+                this_extraction_time,
+                mock_client,
+                mock_db,
+                bucketname="test_bucket",
+            )  # noqa
             assert "Successfully written to bucket!" in caplog.text
-
-
